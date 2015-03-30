cfdb08b3bdcd283bc8636b0547b93f44c80e5e88
Liam McCartney <liam.mccartney@gmail.com>
2015-03-30 12:41:42 -0400

    rms console.log

--- selection_filter.js
+++ selection_filter.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   //Binds sorting function to click event
   $(".plotdiv").on('click', ".bk-ui-state-default.bk-slick-header-column.bk-ui-sortable-handle", function(e){
     if (this.id.match(/slickgrid_\d+_checkbox_selector/) != null) {
-      console.log('hey')
       var columnData = $(this).data("column")
       var dataTableView = findViewObject(tableElement(this), Bokeh.index[modelid])
       selectionSort(dataTableView, columnData)
