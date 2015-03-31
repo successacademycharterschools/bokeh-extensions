16f6a8b6ffa97d6d7f1d09448838ecf89d6049fd
Liam McCartney <liam.mccartney@gmail.com>
2015-03-31 12:02:20 -0400

    adds method to get unique options from columns

--- value_sorting.js
+++ value_sorting.js
@@ -4,4 +4,23 @@ $(document).ready(function(){
   var fields = dataSource.attributes.column_names
 
   $(".plotdiv").append("<form id='column-filters'><input type='submit'></form>")
-})
+var optionsConstructor = function(source, fieldName){
+  var result = "<option value=''></option>";
+  var elements = getUniqueElements(source.attributes.data[fieldName])
+  for(var i = 0; i < elements.length; i++){
+    result += "<option value="+ elements[i] +">" + elements[i] + "</option>"
+  }
+  return result
+}
+
+var getUniqueElements = function(array){
+  var u = {}, a = [];
+     for(var i = 0; i < array.length; ++i){
+        if(u.hasOwnProperty(array[i])) {
+           continue;
+        }
+        a.push(array[i]);
+        u[array[i]] = 1;
+     }
+     return a;
+}
